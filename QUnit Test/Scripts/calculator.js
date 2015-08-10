876b72a662572716b4a3a8c528fcea3689649900
mbyrne77 <mbyrne77@comcast.net>
2015-08-09 22:52:29 -0400

    first checking

---
+++ QUnit Test/Scripts/calculator.js
@@ -0,0 +1,15 @@
+﻿var calculator = function calculator() {
+
+    var add = function add(x, y) {
+            return x + y;
+        },
+        subtract = function subtract(x, y) {
+            return x - y;
+        };
+
+    return {
+        add: add,
+        subtract: subtract
+    };
+
+}();
---
+++ QUnit Test/Scripts/tests.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="qunit.js" />
+/// <reference path="calculator.js" />
+
+QUnit.test("hello test", function (assert) {
+    assert.ok(1 == "1", "Passed!");
+});
+
+QUnit.test("Add", function(assert) {
+    assert.strictEqual(calculator.add(1, 1), 2, "Add Passed!");
+});
+
+QUnit.test("Subtract", function(assert) {
+    assert.strictEqual(calculator.subtract(1, 1), 0, "Subtract Passed!");
+});
